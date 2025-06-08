f4625b57b038dcdbe304433530e38294feb14980
Alex Khakimov <alexanderkhakimov@gmail.com>
2025-06-08 14:15:30 +0300

    все эндпойты работают

--- src/main/java/com/example/market/controller/CartController.java
+++ src/main/java/com/example/market/controller/CartController.java
@@ -1,20 +1,22 @@
 package com.example.market.controller;
 
+import com.example.market.helper.CartAction;
 import com.example.market.model.Item;
 import com.example.market.model.Order;
 import com.example.market.repository.ItemRepository;
 import com.example.market.repository.OrderRepository;
+import com.example.market.service.CartService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 @Controller
 public class CartController {
@@ -22,24 +24,47 @@ public class CartController {
     private final ItemRepository itemRepository;
     @Autowired
     private final OrderRepository orderRepository;
+    @Autowired
+    private final CartService cartService;
 
-    public CartController(ItemRepository itemRepository, OrderRepository orderRepository) {
+    public CartController(ItemRepository itemRepository, OrderRepository orderRepository, CartService cartService) {
         this.itemRepository = itemRepository;
         this.orderRepository = orderRepository;
+        this.cartService = cartService;
     }
 
     @GetMapping("/cart/items")
     public String getCartPage(Model model) {
-      Order cart = orderRepository.findByStatus("CART")
-              .orElseThrow(()->new IllegalArgumentException("Корзина пустая!"));
+        Order cart = orderRepository.findByStatus("CART")
+                .orElseThrow(() -> new IllegalArgumentException("Корзина пустая!"));
         BigDecimal total = BigDecimal.ZERO;
-        if(cart != null){
-            total=cart.totalSum();
+        if (cart != null) {
+            total = cart.totalSum();
         }
         model.addAttribute("items", cart != null ? cart.getOrderItems() : new ArrayList<>());
         model.addAttribute("total", total);
         model.addAttribute("empty", cart == null || cart.getOrderItems().isEmpty());
 
         return "cart";
+    }
+
+    @PostMapping("/cart/items/{id}")
+    public String updateCart(
+            @PathVariable Long id,
+            @RequestParam("action") String action) {
+        cartService.updateCart(id, CartAction.valueOf(action.toUpperCase()));
+        return "redirect:/cart/items";
+    }
+
+    @PostMapping("/buy")
+    public String buy() {
+        return orderRepository.findByStatus("CART")
+                .filter(cart -> !cart.getOrderItems().isEmpty())
+                .map(cart -> {
+                    cart.setStatus("PENDING");
+                    orderRepository.save(cart);
+                    return "redirect:/orders/" + cart.getId() + "?newOrder=true";
+                })
+                .orElse("redirect:/cart/items");
     }
 }
--- src/main/java/com/example/market/controller/OrderController.java
+++ src/main/java/com/example/market/controller/OrderController.java
@@ -1,4 +1,40 @@
 package com.example.market.controller;
 
+import com.example.market.model.Order;
+import com.example.market.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class OrderController {
+    @Autowired
+    private final OrderRepository orderRepository;
+
+    public OrderController(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @GetMapping("/orders")
+    public String getOrdersPage(Model model) {
+        model.addAttribute("orders", orderRepository.findAllByStatusNot("CART"));
+        return "orders";
+    }
+
+    @GetMapping("/orders/{id}")
+    public String getOrderPage(@PathVariable Long id,
+                               @RequestParam(defaultValue = "false") boolean newOrder,
+                               Model model) {
+
+        Order order = orderRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Заказ не найден"));
+        model.addAttribute("order", order);
+        model.addAttribute("newOrder", newOrder);
+
+        return "order";
+    }
+
 }
